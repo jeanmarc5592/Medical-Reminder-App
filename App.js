6fb66966208ba4f0f3a28fc0a9b87bafe6af54c0
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-13 12:48:59 +0200

    Add global Theme to App

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeProvider } from "react-native-elements";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,15 +7,23 @@ import { LoginScreen, SignupScreen } from './src/screens';
 
 const Stack = createStackNavigator();
 
+const theme = {
+  background: {
+    primary: "#EFFAFF",
+  },
+};
+
 const App = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator headerMode={false}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={SignupScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator headerMode={false}>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </SafeAreaProvider>
   );  
 }
