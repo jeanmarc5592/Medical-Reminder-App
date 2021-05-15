eb8623222a5c646f886baae719a7e39556e96cd5
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-15 23:21:56 +0200

    Create CustomInput Component

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Input, withTheme } from 'react-native-elements';
+
+const CustomInput = ({ theme, inputStyle = {}, leftIconContainerStyle = {}, containerStyle = {}, ...inputProps}) => {
+    
+    return (
+        <Input 
+            inputStyle={[{ color: theme.text.dark }, inputStyle]} 
+            containerStyle={[{ marginBottom: 20 }, containerStyle]}
+            leftIconContainerStyle={[{ marginRight: 10 }, leftIconContainerStyle]}
+            {...inputProps} 
+        />
+    )
+};
+
+export default withTheme(CustomInput);
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Screen } from './Screen';
 export { default as CustomText } from './CustomText';
+export { default as CustomInput } from './CustomInput';
