5382a925afa7c05c2a833e7c11a40c2cac32083e
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-18 17:47:25 +0200

    Extend SignupScreen

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import { View, TouchableOpacity } from 'react-native';
 import { Button, withTheme } from "react-native-elements";
 import { Screen, CustomText, CustomInput, CustomButton } from "../components";
 
@@ -10,28 +11,20 @@ const SignupScreen = ({ navigation, theme }) => {
       <CustomText h2 fontWeight="bold" style={{ marginTop: 50 }}>
         Let's start!
       </CustomText>
-      <CustomText style={{ marginTop: 15, fontSize: 18, marginBottom: 50 }}>
-        Sign up for a free account
-      </CustomText>
-      <CustomInput 
-        leftIcon={<FontAwesome name="user" size={24} color={theme.text.dark} />} 
-        placeholder="Your Name"
-      />
-      <CustomInput 
-        leftIcon={<MaterialIcons name="email" size={24} color={theme.text.dark} />} 
-        placeholder="Your Email"
-      />
-      <CustomInput 
-        leftIcon={<MaterialIcons name="lock" size={24} color={theme.text.dark} />} 
-        placeholder="Your Password" 
-        secureTextEntry
-      />
-      <CustomInput 
-        leftIcon={<MaterialIcons name="lock" size={24} color={theme.text.dark} />} 
-        placeholder="Repeat your Password" 
-        secureTextEntry
-      />
+      <CustomText style={{ marginTop: 15, fontSize: 18, marginBottom: 50 }}>Sign up for a free account</CustomText>
+      <CustomInput leftIcon={<FontAwesome name="user" size={24} color={theme.text.dark} />} placeholder="Your Name" />
+      <CustomInput leftIcon={<MaterialIcons name="email" size={24} color={theme.text.dark} />} placeholder="Your Email" />
+      <CustomInput leftIcon={<MaterialIcons name="lock" size={24} color={theme.text.dark} />} placeholder="Your Password" secureTextEntry />
+      <CustomInput leftIcon={<MaterialIcons name="lock" size={24} color={theme.text.dark} />} placeholder="Repeat your Password" secureTextEntry />
       <CustomButton onPress={() => console.log("Sign up!")} title="Sign up" />
+      <View style={{ display: "flex", flexDirection: "row", marginTop: 40 }}>
+        <CustomText>Already have an account?&nbsp;</CustomText>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <CustomText style={{ textDecorationLine: "underline" }} fontWeight="bold">
+            Log In!
+          </CustomText>
+        </TouchableOpacity>
+      </View>
     </Screen>
   );
 };
