4ce416e750d6c3bfe5f76eb358cceefae1bcdab3
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-24 22:11:28 +0200

    Style ResolveAuthScreen

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -4,21 +4,26 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { withTheme } from 'react-native-elements';
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-const Screen = ({ theme, children, style: customStyles = {} }) => {
-    const defaultStyles = {
+const Screen = ({ theme, children, containerStyle: customContainerStyles = {}, style: customInnerStyles = {} }) => {
+    const defaultContainerStyles = {
       flex: 1,
       backgroundColor: theme.background.primary,
       paddingHorizontal: 20
     };
-    const styles = [defaultStyles, customStyles]
+    const defaultInnerStyles = {
+      alignItems: "center",
+      flex: 1,
+      width: "100%",
+    };
+    const containerStyles = [defaultContainerStyles, customContainerStyles];
+    const innerStyles = [defaultInnerStyles, customInnerStyles];
 
     return (
-        <SafeAreaView style={styles}>
+        <SafeAreaView style={containerStyles}>
             <KeyboardAwareScrollView 
                 showsVerticalScrollIndicator={Platform.OS === "android" ? true : false}
                 bounces={false} 
-                contentContainerStyle={{ alignItems: "center" }} 
-                style={{ flex: 1, width: "100%" }} 
+                contentContainerStyle={innerStyles}  
                 extraHeight={160}
             >
                 {children}
--- src/screens/ResolveAuthScreen.jsx
+++ src/screens/ResolveAuthScreen.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import { ActivityIndicator } from 'react-native';
+import { withTheme } from 'react-native-elements';
 import * as firebase from 'firebase';
-import { Screen } from '../components';
+import { Screen, CustomText } from '../components';
 
-const ResolveAuthScreen = ({ navigation }) => {
+const ResolveAuthScreen = ({ navigation, theme }) => {
   // Check for Authentication Persistance or any other Auth Changes 
   useEffect(() => {
     const cleanup = () => {
@@ -21,10 +22,13 @@ const ResolveAuthScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <Screen>
-      <ActivityIndicator />
+    <Screen style={{ justifyContent: "center" }}>
+      <CustomText h2 fontWeight="bold" style={{ marginBottom: 20 }}>
+        Opening ...
+      </CustomText>
+      <ActivityIndicator color={theme.text.dark} size="large" />
     </Screen>
   );
 }
 
-export default ResolveAuthScreen
+export default withTheme(ResolveAuthScreen);
