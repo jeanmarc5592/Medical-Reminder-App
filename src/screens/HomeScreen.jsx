6b886ec7dddb74827402b29253cb5184924cb770
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-26 00:01:50 +0200

    Add User Credentials to HomeScreen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Alert } from 'react-native';
+import { Alert, View } from 'react-native';
+import { withTheme } from 'react-native-elements';
 import { signUserOut, getUser } from '../api/firebase';
 import { Screen, CustomText, CustomButton } from '../components';
 import { setUserData } from '../actions/user';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation, theme }) => {
     const dispatch = useDispatch();
     const { user }= useSelector(state => state);
 
@@ -14,7 +15,7 @@ const HomeScreen = () => {
         const onSuccess = userData => dispatch(setUserData(userData));
         const showAlert = () => {
             Alert.alert(
-                'Something wrent wrong with getting your data. You will be logged out safely',
+                'Something wrent wrong getting your data. You will be logged out safely',
                 '',
                 [ { text: "Ok", onPress: () => signUserOut(), style: "cancel" } ],
             )
@@ -23,11 +24,18 @@ const HomeScreen = () => {
     }, []);
     
     return (
-        <Screen>
-            <CustomText>Hello {user?.name}</CustomText>
-            <CustomButton title="Log Out" onPress={signUserOut}  />
-        </Screen>
-    )
+      <Screen>
+        <View style={{ alignSelf: "flex-start", width: "100%", display: "flex", flexDirection: "row", alignItems: "center", marginTop: 15 }}>
+          <CustomText h4 fontWeight="bold">
+            Hello {user?.name}!
+          </CustomText>
+          <View style={{ marginLeft: "auto", backgroundColor: theme.text.dark, borderRadius: "50%", height: 50, width: 50, display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <CustomText style={{ color: theme.background.white, fontSize: 25 }}>{user?.name?.[0]}</CustomText>
+          </View>
+        </View>
+        <CustomButton title="Log Out" onPress={signUserOut} />
+      </Screen>
+    );
 }
 
-export default HomeScreen
+export default withTheme(HomeScreen);
