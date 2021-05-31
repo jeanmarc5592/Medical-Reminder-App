39497074d53f79d427315fd65db8a35ad4749db7
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-05-31 12:57:38 +0200

    Add marginBottom to Calendar's Title

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -25,7 +25,7 @@ const Calendar = ({ theme }) => {
 
     return (
       <>
-        <CustomText style={{ fontSize: 18, alignSelf: "flex-start", marginTop: 25, }} fontWeight="medium">
+        <CustomText style={{ fontSize: 18, alignSelf: "flex-start", marginTop: 25, marginBottom: 10 }} fontWeight="medium">
             {date?.toLocaleString("en-US", { month: "long", timeZone: "UTC" }).toUpperCase()}, {date?.getFullYear()}
         </CustomText>
         <View style={{ flexDirection: "row", justifyContent: "space-between", width: "100%", marginBottom: 25 }}>
