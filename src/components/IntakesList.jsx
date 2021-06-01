cad081e763764b4771ef7cc46c01101809c0f6c1
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-06-01 13:26:21 +0200

    Get Intakes from redux store

--- src/components/IntakesList.jsx
+++ src/components/IntakesList.jsx
@@ -1,61 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { View } from 'react-native';
-import CustomText from './CustomText';
 import Intake from './Intake';
 
-const MOCK_INTAKES_LIST = [
-    { 
-        id: 1,
-        name: "Amoxicillin",
-        type: "Capsule",
-        dose: "300mg",
-        amount: 1,
-        frequency: "daily",
-        reminder: "08:30 AM",
-        start: "09/01/2020",
-        end: "02/31/2021"
-    },
-    { 
-        id: 12,
-        name: "Ibuprofen",
-        type: "Tablet",
-        dose: "600mg",
-        amount: 1,
-        frequency: "daily",
-        reminder: "11:30 AM",
-        start: "09/01/2020",
-        end: "02/31/2021"
-    },
-    { 
-        id: 13,
-        name: "Vitamin D3",
-        type: "Drops",
-        dose: "1000mg",
-        amount: 5,
-        frequency: "daily",
-        reminder: "08:30 PM",
-        start: "09/01/2020",
-        end: "02/31/2021"
-    },
-    { 
-        id: 14,
-        name: "Vitamin C",
-        type: "Capsule",
-        dose: "100mg",
-        amount: 1,
-        frequency: "daily",
-        reminder: "09:00 PM",
-        start: "09/01/2020",
-        end: "02/31/2021"
-    }
-]
-
 const IntakesList = () => {
+    const { reminders } = useSelector(state => state.user);
+
+    if (!reminders) {
+        return <></>;
+    }
+
     return (
         <View style={{ marginTop: 35, width: "100%" }}>
             {
-                MOCK_INTAKES_LIST.map(intake => {
-                    return <Intake key={intake.id} {...intake} />
+                reminders.map((intake, index) => {
+                    return <Intake key={`${intake.name}-${index}`} {...intake} />
                 })
             }
         </View>
