6fe97e704541f1c7d2a4ba9d15df22b1ebc426d8
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-06-05 13:15:36 +0200

    Integrate DateTimePickerModal

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useMemo, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { ScrollView } from 'react-native'
-import { CustomInput, CustomText, CustomButton } from '../components'
+import { ScrollView, TouchableOpacity, Appearance } from 'react-native'
+import CustomInput from './CustomInput'
+import CustomText from './CustomText'
+import CustomButton from './CustomButton'
 import { withTheme } from 'react-native-elements'
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import uuid from "react-native-uuid";
 
 const initialAddState = {
@@ -22,6 +25,7 @@ const Form = ({ theme, type = "Add" }) => {
     }, []);
     const [formState, setFormState] = useState(initialState);
     const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [datePickerVisible, setDatePickerVisible] = useState(false);
 
     useEffect(() => {
         if (type === "Edit") {
@@ -48,8 +52,18 @@ const Form = ({ theme, type = "Add" }) => {
         }
     }
 
+    const handleDatePickerConfirm = date => {
+        const formattedTimeString = date?.toLocaleString("en-US", { hour: "2-digit", minute: "2-digit", formatMatcher: "basic" });
+        setFormState({ ...formState, reminder: formattedTimeString });
+        hideDatePicker();
+    }
+
+    const showDatePicker = () => setDatePickerVisible(true);
+
+    const hideDatePicker = () => setDatePickerVisible(false);
+
     return (
-      <ScrollView style={{ width: "100%" }} contentContainerStyle={{ paddingHorizontal: 25 }} bounces={false}>
+      <ScrollView style={{ width: "100%" }} contentContainerStyle={{ paddingHorizontal: 25 }} bounces={false} showsVerticalScrollIndicator={false}>
         {/* *** NAME *** */}
         <CustomText style={{ fontSize: 18, width: "100%", marginLeft: 8, color: theme.text.light }} fontWeight="medium">
           Name* (e.g. Ibuprofen)
@@ -98,14 +112,33 @@ const Form = ({ theme, type = "Add" }) => {
         <CustomText style={{ fontSize: 18, width: "100%", marginLeft: 8, marginTop: 5, color: theme.text.light }} fontWeight="medium">
           Reminder*
         </CustomText>
-        <CustomInput
-          containerStyle={{ width: "100%", marginRight: "auto" }}
-          onChangeText={(name) => setFormState({ ...formState, name })}
-          value={formState.reminder}
-          placeholder="Reminder"
-          autoCorrect={false}
+        <TouchableOpacity 
+            onPress={showDatePicker}
+            style={{ 
+                width: "40%", 
+                alignItems: "center", 
+                marginLeft: 8, 
+                marginTop: 10, 
+                marginBottom: 30, 
+                borderWidth: 1, 
+                borderColor: theme.text.light, 
+                borderRadius: 7, 
+                paddingVertical: 10, 
+                paddingHorizontal: 10 
+            }}
+        >
+            <CustomText style={{ fontSize: 16 }} fontWeight="medium">{formState.reminder}</CustomText>
+        </TouchableOpacity>
+        <DateTimePickerModal 
+            textColor="black"
+            isDarkModeEnabled={Appearance.getColorScheme() === "dark"}
+            display="spinner"
+            isVisible={datePickerVisible} 
+            mode="time" 
+            onConfirm={handleDatePickerConfirm} 
+            onCancel={hideDatePicker}
         />
-        <CustomButton disabled={buttonDisabled} title="Save Medicine" onPress={submitForm} />
+        <CustomButton disabled={buttonDisabled} title="Save Medicine" onPress={submitForm} containerStyle={{ marginBottom: 25 }} />
       </ScrollView>
     );
 }
