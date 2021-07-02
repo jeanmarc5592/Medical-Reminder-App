e0152e1d10405ac042bff1bc3c847971fc38dd26
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2021-07-02 08:53:12 +0200

    Add JS Cods to secureStore API

--- src/api/secureStore.js
+++ src/api/secureStore.js
@@ -1,11 +1,26 @@
 import * as SecureStore from 'expo-secure-store';
 
-// TODO: ADD JS DOCS
-
+/**
+ * ***********************************
+ * **** Stores a value with a key ****
+ * ***********************************
+ * @param {String} key - Key for the stored value
+ * @param {String} value - Value that should be stored
+ * @returns {Void} - Nothing
+ */
 export const saveToSecureStore = async (key = "", value = "") => {
     await SecureStore.setItemAsync(key, value);
 }
 
+
+
+/**
+ * ***********************
+ * **** Reads a value ****
+ * ***********************
+ * @param {String} key - Key that should be read 
+ * @returns {Any} - The stored value for the provided key (if stored)
+ */
 export const getFromSecureStore = async (key = "") => {
     return await SecureStore.getItemAsync(key);
 };
